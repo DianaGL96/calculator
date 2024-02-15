a2559b612502a192870513009fe78bf420ab508d
DianaGL96 <gorina.diana1996@gmail.com>
2024-02-15 11:20:49 +0300

    changes

--- src/main/java/ru/diana/calc/service/ResultWriterService.java
+++ src/main/java/ru/diana/calc/service/ResultWriterService.java
@@ -1,8 +1,7 @@
 package ru.diana.calc.service;
 
 public class ResultWriterService {
-    public static int getResult (String operation, int num1, int num2, int result) {
+    public static void getResult (String operation, int num1, int num2, int result) {
         System.out.println("Операция: " + operation + " - число 1: " + num1 + ", число 2: " + num2 + ", результат: " + result);
-        return result;
     }
 }
--- src/main/java/ru/diana/calc/util/Calculator.java
+++ src/main/java/ru/diana/calc/util/Calculator.java
@@ -2,18 +2,15 @@ package ru.diana.calc.util;
 
 public class Calculator {
     public static int getSum (int num1, int num2) {
-        int sum = num1 + num2;
-        return sum;
+        return num1 + num2;
     }
 
     public static int getDifference (int num1, int num2) {
-        int difference = num1 - num2;
-        return difference;
+        return num1 - num2;
     }
 
     public static int getProduct (int num1, int num2) {
-        int product = num1 * num2;
-        return product;
+        return num1 * num2;
     }
 
 }
